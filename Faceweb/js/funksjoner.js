3c87aeab88229cbde4eee21ecff994d61cba1fb6
teo23062 <144096670+teo23062@users.noreply.github.com>
2024-02-27 08:35:26 +0100

    Opplastning

---
+++ Faceweb/js/funksjoner.js
@@ -0,0 +1,10 @@
+function sur()
+    {
+    document.getElementById('bilde').src="./img/sur.jpg";    
+    }
+
+function trist()
+    {
+    document.getElementById('bilde').src="./img/trist.jpg";    
+    }
+
