0e542f93a9ae0cf7b4e3cb2021eb7c9be321a294
Moldvaer <143492815+Moldvaer@users.noreply.github.com>
2024-02-27 09:54:42 +0100

    syver

--- Faceweb/js/funksjoner.js
+++ Faceweb/js/funksjoner.js
@@ -7,4 +7,21 @@ function trist()
     {
     document.getElementById('bilde').src="./img/trist.jpg";    
     }
+// utvikler 2
+//
+    function spro()
+    {
+    document.getElementById('bilde').src="./img/spro.jpg";    
+    }
 
+function sint()
+    {
+    document.getElementById('bilde').src="./img/sint.jpg";    
+    }
+
+function random ()
+{
+    var tekst 
+    
+}
+    
