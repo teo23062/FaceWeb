c6b0f9b7c3f12f80a85b0ba69936e0559ae143dc
teo23062 <144096670+teo23062@users.noreply.github.com>
2024-02-27 09:53:54 +0100

    Utvikler 1

--- Faceweb/js/funksjoner.js
+++ Faceweb/js/funksjoner.js
@@ -7,4 +7,29 @@ function trist()
     {
     document.getElementById('bilde').src="./img/trist.jpg";    
     }
+    
 
+// Utvikler 1
+function glad()
+    {
+    document.getElementById('bilde').src="./img/glad.jpg";    
+    }
+
+function redd()
+    {
+    document.getElementById('bilde').src="./img/redd.jpg";    
+    }
+
+var bilder = ["./img/glad.jpg", "./img/redd.jpg", "./img/sint.jpg", "./img/spro.jpg", "./img/sur.jpg", "./img/trist.jpg"];
+var i = 0;
+
+function tilfeldig() {
+    var max = 5;
+    var min = 0;
+
+    i = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (i >= bilder.length) {
+        i = 0;
+    }
+    document.getElementById("bilde").src = bilder[i];
+}
